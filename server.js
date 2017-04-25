563324cacde87079940a73ed0175ef441ad360b9
j-Sut <flowstategame@gmail.com>
2017-04-25 09:43:46 -0700

    added recipes get endpoint

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -18,10 +19,18 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+Recipes.create('Choc Milk', ['milk', 'cocoa powder']);
+Recipes.create('Morning Coffee', ['coffee grounds', 'creamer', 'coconut oil']);
+Recipes.create('Veg Stirfry', ['broccoli', 'asperagus', 'bell pepper', 'garlic', 'soy sauce', 'a can-do attituded']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res)=> {
+	res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
